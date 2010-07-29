77dd06467286f644e6d4d9c108f5917e9e763d42
sergeyvorobyev@google.com <sergeyvorobyev@google.com@500ff770-ffb2-11de-9d03-c52165109085>
2010-07-29 12:45:10 +0000

    Test git commit

--- src/org/jtsan/Agent.java
+++ src/org/jtsan/Agent.java
@@ -89,7 +89,6 @@ public class Agent implements ClassFileTransformer {
     "java/nio/",
     "java/util/Arrays",
 
-
     // Exclude some internals of java.util.concurrent to avoid false report.
     // "java/util/concurrent/locks/AbstractQueuedSynchronizer",
     // "java/util/concurrent/locks/LockSupport",
