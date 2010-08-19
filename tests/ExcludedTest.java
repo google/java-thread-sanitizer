4b11172beff1d789f060dd49f3c17e50794d5d52
sergeyvorobyev@google.com <sergeyvorobyev@google.com@500ff770-ffb2-11de-9d03-c52165109085>
2010-08-19 14:44:24 +0000

    Rename @Disable to @ExcludedTest

--- tests/ExcludedTest.java
+++ tests/ExcludedTest.java
@@ -24,6 +24,6 @@ import java.lang.annotation.Target;
 
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.METHOD})
-public @interface Disable {
+public @interface ExcludedTest {
   String reason() default "N/A";
 }
